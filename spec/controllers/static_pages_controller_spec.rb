3cb23bfee295c1429d3239ba18ca41e2d130410c
Serguei Alleko <ask@alleko.com>
2014-07-09 15:04:35 -0400

    rspec for devise session returns wrong template

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,11 +2,36 @@ require 'spec_helper'
 
 describe StaticPagesController, :type => :controller do
 
+  render_views
+
   describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
+    it "contains welcome message" do
+      get :home
+      expect(response.body).to match /Welcome to Chores!/m
     end
   end
+
+  describe "GET 'contact'" do
+    it "contains contact info" do
+      get :contact
+      expect(response.body).to match /Contact Chores team/m
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "contains about info" do
+      get :about
+      expect(response.body).to match /About/m
+    end
+  end
+
+  describe "sign out" do
+    it "redirects to home page" do
+      get :sign_out
+      expect(current_user).should be_nil
+      expect(response).to render_template("index")
+    end
+  end
+
 
 end
