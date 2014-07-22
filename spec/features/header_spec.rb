851046192c433adf6571c991321b36b7e1f1e3c0
Serguei Alleko <ask@alleko.com>
2014-07-22 01:32:58 -0400

    feature specs for login/logout

--- spec/features/header_spec.rb
+++ spec/features/header_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
 describe "Header", :type => :feature do
-  describe "checking header at Home page" do
-   it "when user is not logged in, it should contain link to home and log in" do
-      visit '/'
-      expect(page).to have_link('Log In', href: new_user_session_path)
-      expect(page).to have_link('Chores', href: root_path)
-    end
+  context "user is not logged in" do
+   describe "checking header at Home page" do
+    it "it should contain link to home and log in" do
+       visit '/'
+       expect(page).to have_link('Log In', href: new_user_session_path)
+       expect(page).to have_link('Chores', href: root_path)
+     end
+   end
+ end
+
+ context "user is logged in" do
+
+  before do
+    @user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in('user_email', :with => @user.email)
+    fill_in('user_password', :with => @user.password)
+    click_button('Sign in')
   end
 
- describe "checking header at Home page after logged in" do
-   it "when user is logged in, it should contain the menu, sign in info and logout link" do
+  describe "checking header at Home page after logged in" do
+    it "it should contain the menu, sign in info and logout link" do
   
-      user = FactoryGirl.create(:user)
-      visit new_user_session_path
-      fill_in('user_email', :with => user.email)
-      fill_in('user_password', :with => user.password)
-      click_button('Sign in')
-
       visit '/'
       expect(page).to have_link('Log Out', href: destroy_user_session_path)
       expect(page).to have_link('Home', href: root_path)
@@ -25,10 +31,17 @@ describe "Header", :type => :feature do
       expect(page).to have_link('Chores', href: chores_path)
       expect(page).to have_link('Projects', href: projects_path)
       expect(page).to have_link('Contexts', href: contexts_path)
-      expect(page).to have_content('Signed in as: '+user.email)
+      expect(page).to have_content('Signed in as: '+@user.email)
     end
+
+    it "when user hits log out, it should not see the Signed as message" do
+     visit('/')
+     click_link("Log Out")
+     expect(page).to_not have_content('Signed in as:')
+     expect(page).to  have_content('Log In')
+    end
+   end
   end
-  
 
 end
 
