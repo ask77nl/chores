7b4acaf93663d981933e7c3e9955b0c2bd58b3b6
Serguei Alleko <ask@alleko.com>
2014-07-25 14:50:56 -0400

    fixed footer

--- spec/features/footer_spec.rb
+++ spec/features/footer_spec.rb
@@ -5,10 +5,10 @@ describe "Header", :type => :feature do
    it "when user is not logged in, it should contain some footer links" do
       visit '/'
       print page.body
-      expect(page).to have_link('Chores', href: "http://chores.com/")
+      expect(page).to have_link('Chores')
       expect(page).to have_link('About', href: about_page_path)
       expect(page).to have_link('Contact', href: contact_page_path)
-      expect(page).to have_link('News', href: "http://news.chores.com")
+      expect(page).to have_link('News')
     end
   end
 
