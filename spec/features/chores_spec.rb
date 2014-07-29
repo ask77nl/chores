9f5e8dc44a6aa72b3b5756f2df69982f78419db3
Serguei Alleko <ask@alleko.com>
2014-07-28 22:51:25 -0400

    finished basic tests except deadlines and schedules

--- spec/features/chores_spec.rb
+++ spec/features/chores_spec.rb
@@ -8,7 +8,7 @@ describe "Chores", :type => :feature do
     @user = FactoryGirl.create(:user)
     @context = FactoryGirl.create(:context, user_id: @user.id)
     @project = FactoryGirl.create(:project, context_id: @context.id, user_id: @user.id)
-    @choretype = FactoryGirl.create(:choretype, user_id: @user.id)
+    @choretype = FactoryGirl.create(:choretype)
     @email = FactoryGirl.create(:email, user_id: @user.id)
     visit new_user_session_path
     fill_in('user_email', :with => @user.email)
@@ -43,98 +43,116 @@ describe "Chores", :type => :feature do
       fill_in('chore_title', :with => chore.title)
       select(@email.subject,:from => 'chore[email_id]')
       select(@choretype.name,:from => 'chore[choretype_id]')
-      select(@project.subject,:from => 'chore[project_id]')
+      select(@project.title,:from => 'chore[project_id]')
       
       click_button('Create Chore')
 
       expect(page).to have_content('Chore was successfully created.')
       expect(page).to have_content('Title: '+chore.title)
+      expect(page).to have_content('Email: '+@email.subject)
+      expect(page).to have_content('Type: '+@choretype.name)
+      expect(page).to have_content('Project: '+@project.title)
       
       
       visit chores_path
      
       expect(page).to have_content(chore.title)
       expect(page).to have_content(@context.name)
+      expect(page).to have_content(@email.subject)
+      expect(page).to have_content(@choretype.name)
+      expect(page).to have_content(@project.title)
+
       
     end
    end
 
-   describe "when many users create chore" do
-    it "should show each users only his chores" do
-      chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id)
+describe "when there are chores of several contexts and types" do
+    it "should filter chores by context and type" do
 
-     other_user = FactoryGirl.create(:user)
-     other_chore = FactoryGirl.create(:chore,user_id: other_user.id, context_id: @context.id)
+    other_context = FactoryGirl.create(:context, user_id: @user.id)
+    other_project = FactoryGirl.create(:project, context_id: other_context.id, user_id: @user.id)
+    other_choretype = FactoryGirl.create(:choretype)
+
+    chore_usual = FactoryGirl.create(:chore, project_id: @project.id, email_id: @email.id, choretype_id: @choretype.id, user_id: @user.id)
    
-     visit chores_path
+    chore_other_context = FactoryGirl.create(:chore, project_id: other_project.id, email_id: @email.id, choretype_id: @choretype.id, user_id: @user.id)
+  
+    chore_other_type = FactoryGirl.create(:chore, project_id: @project.id, email_id: @email.id, choretype_id: other_choretype.id, user_id: @user.id)
 
-     expect(page).to have_content(chore.title)
-     expect(page).to have_no_content(other_chore.title)
+     visit chores_path(:context => @context.id, :choretype =>@choretype.id)
+
+     expect(page).to have_content(chore_usual.title)
+     expect(page).to have_no_content(chore_other_context.title)
+     expect(page).to have_no_content(chore_other_type.title)
+   
+     expect(page).to have_content(other_context.name)
+     click_link(other_context.name)
+  
+     expect(page).to have_no_content(chore_usual.title)
+     expect(page).to have_content(chore_other_context.title)
+     expect(page).to have_no_content(chore_other_type.title)
+ 
+     expect(page).to have_content(other_choretype.name)
+     visit chores_path(:context => @context.id, :choretype =>other_choretype.id)   
+
+     expect(page).to have_no_content(chore_usual.title)
+     expect(page).to have_no_content(chore_other_context.title)
+     expect(page).to have_content(chore_other_type.title)
+     
+   
       
-    end
-  end
-
-   describe "when there is a parent chore" do
-    it "should be visible and available for selection on edit" do
-     
-      parent_chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id) 
-      
-      visit new_chore_path
-     
-      expect(page).to have_select('chore[parent_chore_id]', :options => ['None',parent_chore.title])
-
-      chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id, parent_chore_id: parent_chore.id)
-     
-     visit chores_path
-     expect(page).to have_content(parent_chore.title)
-     expect(page).to have_content(chore.title)
-
-
     end
   end
 
    
    describe "after there are some chores" do
     it "should be able to show it" do
-      chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id)
+     chore = FactoryGirl.create(:chore, project_id: @project.id, email_id: @email.id, choretype_id: @choretype.id, user_id: @user.id)
 
      visit chores_path
      expect(page).to have_content(chore.title)
      click_link("Show")
      
      expect(page).to have_content('Title: '+chore.title)
+     expect(page).to have_content(@email.subject)
+     expect(page).to have_content(@choretype.name)
+     expect(page).to have_content(@project.title)
+
     end
    end
    
     it "should be able to edit it" do
-       parent_chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id)
+     chore = FactoryGirl.create(:chore, project_id: @project.id, email_id: @email.id, choretype_id: @choretype.id, user_id: @user.id)
 
-      chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id)
+     new_project = FactoryGirl.create(:project, context_id: @context.id, user_id: @user.id)
 
      visit chores_path
      expect(page).to have_content(chore.title)
      click_link("Edit", :href =>"/chores/"+chore.id.to_s+"/edit")
    
      expect(find_field('chore_title').value).to  eq chore.title
+     expect(page).to have_select('chore[email_id]', :selected => @email.subject);
+     expect(page).to have_select('chore[choretype_id]', :selected => @choretype.name);
+     expect(page).to have_select('chore[project_id]', :selected => @project.title);
 
      new_title = "new title"
      fill_in('chore_title', :with => new_title)
 
-     select(parent_chore.title,:from => 'chore[parent_chore_id]')
+     select(new_project.title,:from => 'chore[project_id]')
 
      click_button("Update Chore")
  
      expect(page).to have_content('Title: '+new_title)
-     expect(page).to have_content('Parent chore: '+parent_chore.title)
+     expect(page).to have_content('Project: '+new_project.title)
 
      visit chores_path
 
      expect(page).to have_content(new_title)
-     expect(page).to have_content(parent_chore.title)
+     expect(page).to have_content(new_project.title)
    end
 
    it "should be able to delete it" do
-      chore = FactoryGirl.create(:chore, context_id: @context.id, user_id: @user.id)
+     chore = FactoryGirl.create(:chore, project_id: @project.id, email_id: @email.id, choretype_id: @choretype.id, user_id: @user.id)
 
      visit chores_path
      expect(page).to have_content(chore.title)
